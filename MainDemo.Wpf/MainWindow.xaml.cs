2c94549ca3200679a392bff91587fecf43125791
Rey Santana Socorro <reysantana@users.noreply.github.com>
2020-09-28 03:17:22 -0400

    1

---
+++ MainDemo.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using MaterialDesignColors.WpfExample.Domain;
+using MaterialDesignThemes.Wpf;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MaterialDesignColors.WpfExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static Snackbar Snackbar;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(2500);
+            }).ContinueWith(t =>
+            {
+                //note you can use the message queue from any thread, but just for the demo here we 
+                //need to get the message queue from the snackbar, so need to be on the dispatcher
+                MainSnackbar.MessageQueue.Enqueue("Welcome to Material Design In XAML Tookit");
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+
+            DataContext = new MainWindowViewModel(MainSnackbar.MessageQueue);
+
+            PaletteHelper paletteHelper = new PaletteHelper();
+            ITheme theme = paletteHelper.GetTheme();
+
+            DarkModeToggleButton.IsChecked = theme.GetBaseTheme() == BaseTheme.Dark;
+
+            if (paletteHelper.GetThemeManager() is { } themeManager)
+            {
+                themeManager.ThemeChanged += (_, e) =>
+                {
+                    DarkModeToggleButton.IsChecked = e.NewTheme?.GetBaseTheme() == BaseTheme.Dark;
+                };
+            }
+
+            Snackbar = MainSnackbar;
+        }
+
+        private void UIElement_OnPreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //until we had a StaysOpen glag to Drawer, this will help with scroll bars
+            var dependencyObject = Mouse.Captured as DependencyObject;
+            while (dependencyObject != null)
+            {
+                if (dependencyObject is ScrollBar) return;
+                dependencyObject = VisualTreeHelper.GetParent(dependencyObject);
+            }
+
+            MenuToggleButton.IsChecked = false;
+        }
+
+        private async void MenuPopupButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var sampleMessageDialog = new SampleMessageDialog
+            {
+                Message = { Text = ((ButtonBase)sender).Content.ToString() }
+            };
+
+            await DialogHost.Show(sampleMessageDialog, "RootDialog");
+        }
+
+        private void OnCopy(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Parameter is string stringValue)
+            {
+                try
+                {
+                    Clipboard.SetDataObject(stringValue);
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private void MenuToggleButton_OnClick(object sender, RoutedEventArgs e) 
+            => DemoItemsSearchBox.Focus();
+
+        private void MenuDarkModeButton_Click(object sender, RoutedEventArgs e) 
+            => ModifyTheme(DarkModeToggleButton.IsChecked == true);
+
+        private static void ModifyTheme(bool isDarkTheme)
+        {
+            PaletteHelper paletteHelper = new PaletteHelper();
+            ITheme theme = paletteHelper.GetTheme();
+
+            theme.SetBaseTheme(isDarkTheme ? Theme.Dark : Theme.Light);
+
+            paletteHelper.SetTheme(theme);
+        }
+    }
+}
---
+++ MaterialDesignThemes.Wpf/Badged.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+using ControlzEx;
+
+namespace MaterialDesignThemes.Wpf
+{
+    [TemplatePart(Name = BadgeContainerPartName, Type = typeof(UIElement))]
+    public class Badged : BadgedEx
+    {
+        private static readonly CornerRadius DefaultCornerRadius = new CornerRadius(9);
+
+        #region DependencyProperty : BadgeChangedStoryboardProperty
+        public Storyboard BadgeChangedStoryboard
+        {
+            get { return (Storyboard)GetValue(BadgeChangedStoryboardProperty); }
+            set { SetValue(BadgeChangedStoryboardProperty, value); }
+        }
+        public static readonly DependencyProperty BadgeChangedStoryboardProperty
+            = DependencyProperty.Register(nameof(BadgeChangedStoryboard), typeof(Storyboard), typeof(Badged), new PropertyMetadata(default(Storyboard)));
+        #endregion
+
+        #region DependencyProperty : BadgeColorZoneModeProperty
+        public ColorZoneMode BadgeColorZoneMode
+        {
+            get { return (ColorZoneMode)GetValue(BadgeColorZoneModeProperty); }
+            set { SetValue(BadgeColorZoneModeProperty, value); }
+        }
+        public static readonly DependencyProperty BadgeColorZoneModeProperty
+            = DependencyProperty.Register(nameof(BadgeColorZoneMode), typeof(ColorZoneMode), typeof(Badged), new PropertyMetadata(default(ColorZoneMode)));
+        #endregion
+
+        #region DependencyProperty : CornerRadiusProperty
+        public CornerRadius CornerRadius
+        {
+            get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+        public static readonly DependencyProperty CornerRadiusProperty
+            = DependencyProperty.Register(nameof(CornerRadius), typeof(CornerRadius), typeof(Badged), new PropertyMetadata(DefaultCornerRadius));
+        #endregion
+        
+        static Badged()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Badged), new FrameworkPropertyMetadata(typeof(Badged)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            BadgeChanged -= OnBadgeChanged;
+            base.OnApplyTemplate();
+            BadgeChanged += OnBadgeChanged;
+        }
+
+        private void OnBadgeChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (_badgeContainer is null || BadgeChangedStoryboard is null)
+            {
+                return;
+            }
+            
+            _badgeContainer.BeginStoryboard(BadgeChangedStoryboard);
+        }
+    }
+}
